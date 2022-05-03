f7d0c3ba3eb503ea66c980bda8f9ce99f75b9d18
QHP1015 <2439462872@qq.com>
2022-05-03 21:49:06 +0800

    feat:add react hooks

---
+++ 前端体系课/React/robot-gallery/src/AppState.tsx
@@ -0,0 +1,34 @@
+import React, { useState, ReactDOM, ReactChild } from "react";
+
+interface AppStateValue {
+    username: string;
+    shoppingCart: {
+        items: { id: number, name: string }[]
+    }
+}
+
+const defaultContextValue: AppStateValue = {
+    username: 'qhp',
+    shoppingCart: {
+        items: []
+    },
+}
+
+export const appContext = React.createContext(defaultContextValue);
+export const appSetStateContext = React.createContext<React.Dispatch<React.SetStateAction<AppStateValue>> | undefined>(undefined)
+
+interface Props {
+    children: ReactChild
+}
+
+export const AppStateProvider: React.FC<Props> = (props: Props) => {
+    const [state, setState] = useState(defaultContextValue);
+
+    return (
+        <appContext.Provider value={state}>
+            <appSetStateContext.Provider value={setState}>
+                {props.children}
+            </appSetStateContext.Provider>
+        </appContext.Provider>
+    );
+};
--- 前端体系课/React/robot-gallery/src/components/ShoppingCart.tsx
+++ 前端体系课/React/robot-gallery/src/components/ShoppingCart.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./ShoppingCart.module.css";
 import { FiShoppingCart } from "react-icons/fi"
-
+import { appContext } from '../AppState';
 interface Props {
 
 }
@@ -18,30 +18,36 @@ class ShoppingCart extends React.Component<Props, State>{
     }
 
     handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        console.log('e.target:',e.target);
-        console.log('e.currentarget:',e.currentTarget);
-        if((e.target as HTMLElement).nodeName === 'SPAN')
+        console.log('e.target:', e.target);
+        console.log('e.currentarget:', e.currentTarget);
+        if ((e.target as HTMLElement).nodeName === 'SPAN')
             this.setState({ isOpen: !this.state.isOpen });
     }
     render(): React.ReactNode {
         return (
-            <div className={styles.cartContainer}>
-                <button className={styles.button}
-                    onClick={this.handleClick}
-                >
-                    <FiShoppingCart />
-                    <span>购物车2（件）</span>
-                </button>
-                <div className={styles.cartDropDown}
-                    style={{
-                        display: this.state.isOpen ? 'block' : 'none'
-                    }}>
-                    <ul>
-                        <li>robot1</li>
-                        <li>robot2</li>
-                    </ul>
-                </div>
-            </div>
+            <appContext.Consumer>
+                {(value) => {
+                    return (<div className={styles.cartContainer}>
+                        <button className={styles.button}
+                            onClick={this.handleClick}
+                        >
+                            <FiShoppingCart />
+                            <span>购物车{value.shoppingCart.items.length}（件）</span>
+                        </button>
+                        <div className={styles.cartDropDown}
+                            style={{
+                                display: this.state.isOpen ? 'block' : 'none'
+                            }}>
+                            <ul>
+                                {value.shoppingCart.items.map(i => (
+                                    <li>{i.name}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>)
+                }}
+            </appContext.Consumer>
+
         )
     }
 }
