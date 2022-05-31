b201f642949d407a481cb2a5f287da31a0cb3e8f
QHP1015 <2439462872@qq.com>
2022-05-31 09:20:06 +0800

    react

--- 前端体系课/React/react-travel/src/components/sideMenu/SideMenu.tsx
+++ 前端体系课/React/react-travel/src/components/sideMenu/SideMenu.tsx
@@ -1,39 +1,29 @@
-import React from "react";
-import styles from './SideMenu.module.css'
-import { sideMenuList } from './mockup'
-import { Menu } from 'antd'
-import { GifOutlined } from '@ant-design/icons'
+import React from 'react';
+import styles from './SideMenu.module.css';
+import { sideMenuList } from './mockup';
+import { Menu } from 'antd';
+import { GifOutlined } from '@ant-design/icons';
+import { smart } from '@babel/template';
 
 
 export const SideMenu: React.FC = () => {
     return (
-        <Menu mode='vertical' className={styles["side-menu"]}>
+        <Menu mode='vertical' className={styles['side-menu']}>
             {sideMenuList.map((m, index) => (
                 <Menu.SubMenu
                     key={`side-menu-${index}`}
-                    title={
-                        <span>
-                            <GifOutlined />
-                            {m.title}
-                        </span>}
+                    title={<span><GifOutlined />{m.title}</span>}
                 >
                     {m.subMenu.map((sm, smindex) => (
                         <Menu.SubMenu
-                            key={`side-menu-${smindex}`}
-                            title={
-                                <span>
-                                    <GifOutlined />
-                                    {sm.title}
-                                </span>}
+                            key={`sub-menu-${smindex}`}
+                            title={<span><GifOutlined />{sm.title}</span>}
                         >
                             {sm.subMenu.map((sms, smsindex) => (
                                 <Menu.Item
                                     key={`sub-sub-menu-${smsindex}`}
                                 >
-                                    <span>
-                                        <GifOutlined />
-                                        {sms}
-                                    </span>
+                                    <span><GifOutlined />{sms}</span>
                                 </Menu.Item>
                             ))}
                         </Menu.SubMenu>
