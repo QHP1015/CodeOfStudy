2f60ae6eb2015eaf14c9bbf06702c2ff37063174
QHP1015 <2439462872@qq.com>
2023-01-06 16:10:38 +0800

    feat:初识shader

--- Graphic/three-study/src/main/shader/raw/fragment.glsl
+++ Graphic/three-study/src/main/shader/raw/fragment.glsl
@@ -1,4 +1,4 @@
-precision lowp float;
+precision mediump float;
 varying vec2 vUv;
 varying float vElevation;
 
@@ -13,6 +13,6 @@ void main(){
     // 根据UV,取出对应的颜色
     float height = vElevation + 0.05 * 20.0;
     vec4 textureColor = texture2D(uTexture,vUv);
-    textureColor.rgb*=height;
+    textureColor.rgb *= height;
     gl_FragColor = textureColor;
 }
